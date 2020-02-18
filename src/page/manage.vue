03a671c81dfcba49058cd8b03030a617a9ce23b8
Your Name <you@example.com>
2020-02-18 21:56:10 +0800

    00000

--- src/page/manage.vue
+++ src/page/manage.vue
@@ -10,7 +10,7 @@
 						<el-menu-item index="shopList">商家列表</el-menu-item>
 						<el-menu-item index="foodList">食品列表</el-menu-item>
 						<el-menu-item index="orderList">订单列表</el-menu-item>
-						<el-menu-item index="adminList">管理员列表</el-menu-item>
+						<el-menu-item index="adminList">管理员列表1111111</el-menu-item>
 					</el-submenu>
 					<el-submenu index="3">
 						<template slot="title"><i class="el-icon-plus"></i>添加数据</template>
